e1b01a8180322a4e855f232029849659053f29fa
Kamilo Onwer <k_zhamer@hotmail.com>
2020-04-16 19:47:02 -0500

    Pagina proyecto V4.5.4 Se han agregado los portafolios de samuel y camilo.

---
+++ Portafolio SAHMOK Samuel cruz/carga/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+window.setTimeout(function() {
+	"use strict";
+	var contenedor = document.getElementById('contenedor-carga');
+	contenedor.style.visibility = 'hidden';
+	contenedor.style.opacity = '0';
+}, 1000);
